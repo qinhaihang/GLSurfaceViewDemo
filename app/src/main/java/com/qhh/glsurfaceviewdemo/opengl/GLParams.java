7600b3c3ccfbf388251ce4fe2eb0768e05ad5d32
qinhaihang <hai449769034@qq.com>
2019-07-22 23:09:59 +0800

    1、添加GLParams

--- app/src/main/java/com/qhh/glsurfaceviewdemo/opengl/GLParams.java
+++ app/src/main/java/com/qhh/glsurfaceviewdemo/opengl/GLParams.java
@@ -1,5 +1,12 @@
 package com.qhh.glsurfaceviewdemo.opengl;
 
+import static android.opengl.GLES20.GL_LINK_STATUS;
+import static android.opengl.GLES20.glAttachShader;
+import static android.opengl.GLES20.glCreateProgram;
+import static android.opengl.GLES20.glDeleteProgram;
+import static android.opengl.GLES20.glGetProgramiv;
+import static android.opengl.GLES20.glLinkProgram;
+
 /**
  * @author qinhaihang
  * @version $Rev$
@@ -12,6 +19,43 @@ package com.qhh.glsurfaceviewdemo.opengl;
  */
 public class GLParams {
 
+    /**
+     * Links a vertex shader and a fragment shader together into an OpenGL
+     * program. Returns the OpenGL program object ID, or 0 if linking failed.
+     */
+    public static int linkProgram(int vertexShaderId, int fragmentShaderId) {
 
+        // Create a new program object.
+        final int programObjectId = glCreateProgram();
+
+        if (programObjectId == 0) {
+            return 0;
+        }
+
+        // Attach the vertex shader to the program.
+        glAttachShader(programObjectId, vertexShaderId);
+
+        // Attach the fragment shader to the program.
+        glAttachShader(programObjectId, fragmentShaderId);
+
+        // Link the two shaders together into a program.
+        glLinkProgram(programObjectId);
+
+        // Get the link status.
+        final int[] linkStatus = new int[1];
+        glGetProgramiv(programObjectId, GL_LINK_STATUS,
+                linkStatus, 0);
+
+        // Verify the link status.
+        if (linkStatus[0] == 0) {
+            // If it failed, delete the program object.
+            glDeleteProgram(programObjectId);
+
+            return 0;
+        }
+
+        // Return the program object ID.
+        return programObjectId;
+    }
 
 }
